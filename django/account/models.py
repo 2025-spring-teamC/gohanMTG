42a0ca9116477f4cd22e8b472e56a53adc3c000a
kemickoko <kemickokojiji.work@gmail.com>
2025-04-21 12:49:21 +0900

    エラーの可能性がある部分の修正

--- django/account/models.py
+++ django/account/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 #カスタムユーザーモデルを作成
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, email, password=None, name=None, familygroup=None, **extra_fields):
         if not email:
             raise ValueError("メールアドレスは必須です")
         email = self.normalize_email(email)
@@ -30,7 +30,7 @@ class FamilyGroup(models.Model):
     name = models.CharField(verbose_name="グループ名", max_length=50, unique=True, null=False)
     password = models.CharField(verbose_name="合言葉", max_length=100, null=False)
     create_at = models.DateTimeField(verbose_name="登録日", default=timezone.now)
-    updated_at = models.DateTimeField(verbose_name="更新日時", default=timezone.now)
+    updated_at = models.DateTimeField(verbose_name="更新日時", default=timezone.now, auto_now=True)
 
     def __str__(self):
         return self.name
@@ -44,10 +44,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(verbose_name="ユーザー名", max_length=50)
     email = models.EmailField(verbose_name="メールアドレス", max_length=254, unique=True)
     create_at = models.DateTimeField(verbose_name="登録日", default=timezone.now)
-    updated_at = models.DateTimeField(verbose_name="更新日時", default=timezone.now)
+    updated_at = models.DateTimeField(verbose_name="更新日時", default=timezone.now, auto_now=True)
 
-    familygroup = models.ForeignKey(FamilyGroup, verbose_name="グループID",  on_delete=models.CASCADE)
-
+    familygroup = models.ForeignKey(
+        FamilyGroup,
+        verbose_name="グループID",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        )
 
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
