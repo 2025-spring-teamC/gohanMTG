79bc4225ce64c782a86d2c91ec9108da635fd772
kemickoko <kemickokojiji.work@gmail.com>
2025-05-05 14:44:52 +0900

    管理者画面設定

--- django/search/admin.py
+++ django/search/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from search.models import Recipes, Group_recipes, User_recipes
 
-# Register your models here.
+@admin.register(Recipes)
+class RecipesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category_id', 'url', 'created_at', 'updated_at')
+    search_fields = ('name', 'category_id')
+
+
+@admin.register(Group_recipes)
+class GroupRecipesAdmin(admin.ModelAdmin):
+    list_display = ('group', 'recipe', 'user', 'created_at')
+    list_filter = ('group', 'user')
+    search_fields = ('group__name', 'recipe__name', 'user__name')
+
+
+@admin.register(User_recipes)
+class UserRecipesAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe', 'created_at')
+    list_filter = ('user',)
+    search_fields = ('user__name', 'recipe__name')
