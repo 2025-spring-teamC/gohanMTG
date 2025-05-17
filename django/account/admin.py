7d7e815c51b00d8f0de5fcb914ae9dfc1bc3e15e
kemickoko <kemickokojiji.work@gmail.com>
2025-05-18 08:46:19 +0900

    admin画面にicon_codeも表示

--- django/account/admin.py
+++ django/account/admin.py
@@ -5,16 +5,16 @@ from account import models
 @admin.register(models.User)
 class CustomUserAdmin(UserAdmin):
     model = models.User
-    list_display = ("email", "name", "is_staff", "is_active", "familygroup")
+    list_display = ("email", "name", "icon_code", "is_staff", "is_active", "familygroup")
     list_filter = ("is_staff", "is_active", "familygroup")
     fieldsets = (
-        (None, {"fields": ("email", "password", "name", "familygroup")}),
+        (None, {"fields": ("email", "password", "name", "icon_code", "familygroup")}),
         ("権限", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
     )
     add_fieldsets = (
         (None, {
             "classes": ("wide",),
-            "fields": ("email", "name", "password1", "password2", "familygroup", "is_staff", "is_active")}
+            "fields": ("email", "name", "password1", "password2", "icon_code", "familygroup", "is_staff", "is_active")}
         ),
     )
     search_fields = ("email", "name")
